3b05dc1c04eebb18f813cc01e696fc2413899a07
mohd-jafar <142247641+mohd-jafar@users.noreply.github.com>
2024-02-03 02:16:51 -0800

    component1

---
+++ page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewUser from './components/purchaseoverview'
+
+
+function Page() {
+  return (
+    <>
+    <NewUser/>
+    
+    </>
+  )
+}
+
+export default Page
